8994c549ed1154888845d4176873494ca64f9a3e
Cassidy <z034@europa.internal>
2016-09-20 16:59:49 +0100

    Added button with test functionality and testing commits

--- app/src/main/java/com/example/z034/myirishlifeapp/MainActivity.java
+++ app/src/main/java/com/example/z034/myirishlifeapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.z034.myirishlifeapp;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void hellomark(String qqqq)
+    {
+        EditText text = (EditText) findViewById(R.id.editText);
+        text.setText("I told you to leave me alone");
     }
 }
