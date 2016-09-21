f8d79a3e7c8ae70975bec2ceaf8e979e7e5560fd
MichelleMacCormack <michelle.maccormack@irishlife.ie>
2016-09-21 09:21:23 +0100

    QR code stuff, fingers crossed!

--- app/src/main/java/com/example/z034/myirishlifeapp/MainActivity.java
+++ app/src/main/java/com/example/z034/myirishlifeapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.z034.myirishlifeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.qrButton);
+
+        // Capture button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        QRScannerActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
