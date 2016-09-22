cd7d314c444a3a1b4b116f54aa176c42890f071b
trikav99 <tricia.kavanagh@irishlife.ie>
2016-09-22 16:55:02 +0100

    Added contact us button and screen to land at

---
+++ app/src/main/java/com/example/z034/myirishlifeapp/ContactUsActivity.java
@@ -0,0 +1,16 @@
+package com.example.z034.myirishlifeapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by j518 on 22/09/2016.
+ */
+
+public class ContactUsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_us);
+    }
+}
--- app/src/main/java/com/example/z034/myirishlifeapp/MainActivity.java
+++ app/src/main/java/com/example/z034/myirishlifeapp/MainActivity.java
@@ -27,4 +27,10 @@ public class MainActivity extends AppCompatActivity {
         Intent myIntent = new Intent(getApplicationContext(), QRScannerActivity.class);
         startActivity(myIntent);
     }
+    public void contactUsClick(View v) {
+
+        // Start NewActivity.class
+        Intent myIntent = new Intent(getApplicationContext(), ContactUsActivity.class);
+        startActivity(myIntent);
+    }
 }
