297eacae32bf05b03c43b90a5db8ea99641ef239
x339 <andrea.bontempi@irishlife.ie>
2016-09-22 09:10:00 +0100

    login screen completed

---
+++ app/src/main/java/com/example/z034/myirishlifeapp/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.z034.myirishlifeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void LoginButtonClick(View v)
+    {
+        // Do something
+        // Validate pin with middle tier services
+    }
+
+    public void RegisterButtonClick(View v)
+    {
+        // Do something
+        // Do nothing?
+    }
+
+    public void ForgottenPinButtonClick(View v)
+    {
+        // Do something
+        // Do nothing?
+    }
+}
