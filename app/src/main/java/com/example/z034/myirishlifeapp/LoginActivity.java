64a3375df54c0059348c6ce93bc471bcf4130fd5
RonanCassidy <rcassidydev@gmail.com>
2016-09-23 15:25:12 +0100

    Finshed browser activity

--- app/src/main/java/com/example/z034/myirishlifeapp/LoginActivity.java
+++ app/src/main/java/com/example/z034/myirishlifeapp/LoginActivity.java
@@ -14,6 +14,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         userPinField = (EditText) findViewById(R.id.LoginPin);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     public void LoginButtonClick(View v)
