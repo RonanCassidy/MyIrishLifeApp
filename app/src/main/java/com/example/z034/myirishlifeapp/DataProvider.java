359975035f03ca98ccc03096c67469c10791198f
MarkGunning <gunninmm@gmail.com>
2016-09-26 10:31:39 +0100

    Committing Toolbar and Chat

---
+++ app/src/main/java/com/example/z034/myirishlifeapp/DataProvider.java
@@ -0,0 +1,17 @@
+package com.example.z034.myirishlifeapp;
+
+/**
+ * Created by v584 on 22/09/2016.
+ */
+
+public class DataProvider {
+
+    public boolean position;
+    public String message;
+    public DataProvider(boolean position,String message){
+        super();
+        this.position=position;
+        this.message=message;
+    }
+
+}
--- app/src/test/java/com/example/z034/myirishlifeapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/z034/myirishlifeapp/ExampleUnitTest.java
@@ -2,6 +2,7 @@ package com.example.z034.myirishlifeapp;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.*;
 
 /**
